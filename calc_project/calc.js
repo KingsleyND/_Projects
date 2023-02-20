fec8ae2ff59d49a1f6e9666046189d7c0f0fa730
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-02-20 15:39:19 +0100

    finished cancel button

--- calc_project/calc.js
+++ calc_project/calc.js
@@ -28,12 +28,66 @@ const display = document.querySelector('.display-space');
 
 let txt;
 
+let digitsA = "";
+let digitsB = "";
+
+let number1;
+let number2;
+let operator;
+//  operate(operator,number1,number2)
+
+const clear = () =>{
+    display.innerHTML='';
+}
+
 
 buttons.forEach(function (button){ 
     const addText =() => {
         txt =  button.innerText;
-
-        display.append(txt);
+        if (["+","-","÷","*"].includes(txt) ){
+            number1 = parseInt(digitsA)
+            display.append(txt)
+            operator = txt;
+            switch(operator){
+                case "+":
+                    operator = add;
+                    break;
+                case "-":
+                    operator = subtract;
+                    break;
+                case "÷":
+                    operator = divide;
+                    break;
+                case "*":
+                    operator = multiply;
+                    break;
+            }
+            // console.log(operator)
+            
+        }else if (number1===undefined){
+            digitsA+= txt;
+            display.append(txt);
+            
+        }
+        // console.log(`number1=${number1} number2=${number2} operator=${operator}`)
+        if (operator != undefined){
+            if(["+","-","÷","*","="].includes(txt)==false){
+            digitsB+= txt;
+            display.append(txt);
+            // console.log(digitsB)
+            }
+            if(txt == "="){
+                number2 = parseInt(digitsB);
+                 clear;
+                display.innerHTML=  operate(operator,number1,number2)
+                console.log(operate(operator,number1,number2))
+            }else{
+                
+            }
+        }
+        
+        
+        
         
     }
  button.addEventListener("click",addText);
