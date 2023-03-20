40ef523df88ae3e7a3df307444001c6453a6af4e
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-03-20 13:55:13 +0100

    done with library functionality w/o css

--- top-library/app.js
+++ top-library/app.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-plusplus */
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -9,55 +9,59 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-const bookTest = new Book("Booktitle45","Authorbenny", 2434, "Yes");
-myLibrary.push(bookTest);
 
+const books = document.getElementById("books");
+
+let bookNumber = 0
 function addBookToLibrary() {
+  // Get input values
     const bookName = document.getElementById("name").value;
     document.getElementById("name").value = "";
 
     const author = document.getElementById("author").value;
-    document.getElementById("name").value = "";
+    document.getElementById("author").value = "";
 
     const pages = document.getElementById("pages").value;
-    document.getElementById("name").value = "";
+    document.getElementById("pages").value = "";
 
     const read = document.querySelector("input[name='read']:checked").value;
     
     const Book1 = new Book(bookName, author, pages, read);
 
    
-
     myLibrary.push(Book1);
+    
+    // for(let i = 0; i < myLibrary.length; i++){
+
+      const newBook = document.createElement("div");
+      newBook.className = "book-card";
+      books.appendChild(newBook);
+  
+      const bookTitle = document.createElement("p");
+      bookTitle.innerHTML = myLibrary[bookNumber].title;
+      bookTitle.className = "title";
+      newBook.appendChild(bookTitle);
+  
+      const bookAuthor = document.createElement("p");
+      bookAuthor.innerHTML = myLibrary[bookNumber].author;
+      bookAuthor.className = "author";
+      newBook.appendChild(bookAuthor);
+  
+      const bookPages = document.createElement("p");
+      bookPages.innerHTML = myLibrary[bookNumber].pages;
+      bookPages.className = "pages";
+      newBook.appendChild(bookPages);  
+  
+      const readBook = document.createElement("p");
+      readBook.innerHTML = myLibrary[bookNumber].read;
+      readBook.className = "have-read";
+      newBook.appendChild(readBook);
+  
+    bookNumber +=1;
+    }
     console.log(myLibrary);
-  }
+  // }
+  console.log("outside function")
   console.log(myLibrary);
 
 // addBookToLibrary()
-
-const books = document.getElementById("books");
-
-const newBook = document.createElement("div");
-newBook.className = "book-card";
-books.appendChild(newBook);
-
-
-const bookTitle = document.createElement("p");
-bookTitle.innerHTML = "HOV";
-bookTitle.className = "title";
-newBook.appendChild(bookTitle);
-
-const bookAuthor = document.createElement("p");
-bookAuthor.innerHTML = "ZZZZ";
-bookAuthor.className = "author";
-newBook.appendChild(bookAuthor);
-
-const bookPages = document.createElement("p");
-bookPages.innerHTML = "343";
-bookPages.className = "pages";
-newBook.appendChild(bookPages);  
-
-const readBook = document.createElement("p");
-readBook.innerHTML = "YES";
-readBook.className = "have-read";
-newBook.appendChild(readBook);
