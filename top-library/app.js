dc4fa8ee970f5d2e536f9408d28c1ed2681c5410
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-03-21 13:25:45 +0100

    fixed empty border bug

--- top-library/app.js
+++ top-library/app.js
@@ -9,71 +9,76 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+// const books = document.getElementById("books");
+const books = document.createElement("div");
+  books.id = "books";
+  document.body.appendChild(books);
+  if (books.innerHTML === ""){
+    books.style.borderWidth = "0";
+  }
 
-const books = document.getElementById("books");
+  console.log(books.innerHTML);
 
-let bookNumber = 0
+
+
+let bookNumber = 0;
 function addBookToLibrary() {
+  books.style.borderWidth = "1px";
+  
+
   // Get input values
-    const bookName = document.getElementById("name").value;
-    document.getElementById("name").value = "";
+  const bookName = document.getElementById("name").value;
+  document.getElementById("name").value = "";
 
-    const author = document.getElementById("author").value;
-    document.getElementById("author").value = "";
+  const author = document.getElementById("author").value;
+  document.getElementById("author").value = "";
 
-    const pages = document.getElementById("pages").value;
-    document.getElementById("pages").value = "";
+  const pages = document.getElementById("pages").value;
+  document.getElementById("pages").value = "";
 
-    const read = document.querySelector("input[name='read']:checked").value;
-    
-    const Book1 = new Book(bookName, author, pages, read);
+  const read = document.querySelector("input[name='read']:checked").value;
 
-   
-    myLibrary.push(Book1);
-    
-    // for(let i = 0; i < myLibrary.length; i++){
+  const Book1 = new Book(bookName, author, pages, read);
 
-      const newBook = document.createElement("div");
-      newBook.className = "book-card";
-      books.appendChild(newBook);
-  
-      const bookTitle = document.createElement("p");
-      bookTitle.innerHTML = myLibrary[bookNumber].title;
-      bookTitle.className = "title";
-      newBook.appendChild(bookTitle);
-  
-      const bookAuthor = document.createElement("p");
-      bookAuthor.innerHTML = myLibrary[bookNumber].author;
-      bookAuthor.className = "author";
-      newBook.appendChild(bookAuthor);
-  
-      const bookPages = document.createElement("p");
-      bookPages.innerHTML = myLibrary[bookNumber].pages;
-      bookPages.className = "pages";
-      newBook.appendChild(bookPages);  
-  
-      const readBook = document.createElement("p");
-      readBook.innerHTML = myLibrary[bookNumber].read;
-      readBook.className = "have-read";
-      newBook.appendChild(readBook);
-  
-      const removeBook = document.createElement("button");
-      removeBook.innerHTML = "Remove";
-      
-      // readBook.className = "have-read";
-      newBook.appendChild(removeBook);
+  myLibrary.push(Book1);
 
-      removeBook.addEventListener("click", () =>{
-        newBook.remove();
-      })
+  // for(let i = 0; i < myLibrary.length; i++){
 
-    bookNumber +=1;
-    }
-    // const removeBook = document.createElement("p");
-    
-    console.log(myLibrary);
-  // }  
-  console.log("outside function")
-  console.log(myLibrary);
+  const newBook = document.createElement("div");
+  newBook.className = "book-card";
+  books.appendChild(newBook);
 
-// addBookToLibrary()
+  const bookTitle = document.createElement("p");
+  bookTitle.innerHTML = myLibrary[bookNumber].title;
+  bookTitle.className = "title";
+  newBook.appendChild(bookTitle);
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.innerHTML = myLibrary[bookNumber].author;
+  bookAuthor.className = "author";
+  newBook.appendChild(bookAuthor);
+
+  const bookPages = document.createElement("p");
+  bookPages.innerHTML = myLibrary[bookNumber].pages;
+  bookPages.className = "pages";
+  newBook.appendChild(bookPages);
+
+  const readBook = document.createElement("p");
+  readBook.innerHTML = myLibrary[bookNumber].read;
+  readBook.className = "have-read";
+  newBook.appendChild(readBook);
+
+  const removeBook = document.createElement("button");
+  removeBook.innerHTML = "Remove";
+
+  // readBook.className = "have-read";
+  newBook.appendChild(removeBook);
+
+  removeBook.addEventListener("click", () => {
+    newBook.remove();
+  });
+
+  bookNumber += 1;
+  console.log(books.innerHTML);
+}
+
