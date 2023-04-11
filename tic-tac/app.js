2dd3d2d3e46c029821da8f9b6d28a5570a1afc52
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-04-11 22:18:01 +0100

    notifies user of first win condition

--- tic-tac/app.js
+++ tic-tac/app.js
@@ -8,13 +8,24 @@ const Gameboard = {gameboard:["","","",
                             //   "3","4","5"
                             //   "6","7","8"
 const Player = {};
-
+const wC = Gameboard.gameboard;
 const gamePlay = {xo:0, checkWinner:()=>{
-  const winCondition = Gameboard.gameboard;
-                                switch(winCondition){
-                                  case winCondition[0,4,8]="X":
-                                };
-} };
+  const winCondition = {1:[wC[0],wC[4],wC[8]],2:(wC[0],wC[1],wC[2]),3:(wC[0],wC[3],wC[7]),
+                        4:(wC[1],wC[4],wC[7]),5:(wC[2],wC[4],wC[6]),6:(wC[2],wC[5],wC[8]),
+                        7:(wC[3],wC[4],wC[5]),8:(wC[6],wC[7],wC[8])}
+                   console.log(winCondition[1].join())
+              for(let i=1; i<10; i+=1){
+                let inc = 1
+                //seems like inc always checks only a number 1 value
+                if(winCondition[inc].join() === "X,X,X" || winCondition[inc].join() === "O,O,O"){
+                   console.log("SOMEONE WON");
+                }else{console.log(winCondition[inc].join());
+                  inc+=1;
+                }
+              }
+            },
+            
+           };
 
 const gameBox = document.getElementsByClassName("game-box");
 
@@ -23,7 +34,7 @@ const input = document.querySelectorAll(".input");
 
 const addInput = (event) => {
     const boardKey = event.target.getAttribute("data-key");
-    console.log(boardKey);
+    // console.log(boardKey);
 
     // gamePlay.checkWinner();
     
@@ -38,21 +49,30 @@ const addInput = (event) => {
      gamePlay.xo += 1;
      
     }
-    console.log(gamePlay.xo);
-    console.log(event.target.dataset);
-    console.log(event.target.dataset);
+    // console.log(gamePlay.xo);
+    // console.log(event.target.dataset);
+    // console.log(event.target.dataset);
     console.log("-------------------");
     console.log(Gameboard.gameboard);
+    console.log("-------------------");
+    
+
 }
 
 for( let i=0; i<input.length; i+=1){
-    
     input[i].addEventListener("click", addInput);
+    input[i].addEventListener("click", gamePlay.checkWinner);
     
 }
 
 
-
+// switch (gamePlay.winCondition[1]) {
+//   case "X":
+//   case "O":
+//       alert("Alerting");  
+//       break;
+//       default:
+//   console.log("we are out");
    
 
 
