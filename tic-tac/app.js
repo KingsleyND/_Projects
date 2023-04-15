7f00b07339efe5777d66f7ae094821c2b5579072
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-04-15 16:13:08 +0100

    developed start and reset button

--- tic-tac/app.js
+++ tic-tac/app.js
@@ -7,61 +7,75 @@ const Gameboard = {gameboard:["","","",
                             //   "0","1","2"   0,4,8|0,1,2|0,3,6|1,4,7| 2,4,6|2,5,8|3,4,5|6,7,8
                             //   "3","4","5"
                             //   "6","7","8"
-const Player = {};
-// const wC = Gameboard.gameboard;
-const gamePlay = {xo:0, checkWinner:()=>{
-  const wC = Gameboard.gameboard;
-  const winCondition = {1:[wC[0],wC[4],wC[8]],2:[wC[0],wC[1],wC[2]],3:[wC[0],wC[3],wC[6]],
-                        4:[wC[1],wC[4],wC[7]],5:[wC[2],wC[4],wC[6]],6:[wC[2],wC[5],wC[8]],
-                        7:[wC[3],wC[4],wC[5]],8:[wC[6],wC[7],wC[8]]}
-          
-              for(let i=1; i<10; i+=1){
-              
-                if(winCondition[i].join() === "X,X,X" || winCondition[i].join() === "O,O,O"){
-                   console.log("SOMEONE WON");
-                   const gameWon = true;
-                   break
-                }else{  //  console.log(winCondition[i].join()+ i); 
+const Player = { };
+
+const gamePlay = {xo:0,
+                 gameWon:false,
+                 checkWinner:()=>{
+                    const wC = Gameboard.gameboard;
+                    const winCondition = {1:[wC[0],wC[4],wC[8]],2:[wC[0],wC[1],wC[2]],3:[wC[0],wC[3],wC[6]],
+                                          4:[wC[1],wC[4],wC[7]],5:[wC[2],wC[4],wC[6]],6:[wC[2],wC[5],wC[8]],
+                                          7:[wC[3],wC[4],wC[5]],8:[wC[6],wC[7],wC[8]]}
+                            
+                          for(let i=1; i<10; i+=1){
+                          
+                            if(winCondition[i].join() === "X,X,X" || winCondition[i].join() === "O,O,O"){
+                              console.log("SOMEONE WON");
+                               gamePlay.gameWon = true;
+                               Gameboard.gameboard = ["","","","","","","","",""];
+                              break;
+                            }                
+                          }// return gamePlay.gameWon;
+                 },
+                 input :document.querySelectorAll(".input"),
+                 addInput:(event) => {
+                  if(gamePlay.gameWon === false){
+                  const boardKey = event.target.getAttribute("data-key");
                   
+                  if(gamePlay.input[boardKey].innerHTML === ""){
+                      if(gamePlay.xo%2 === 0){
+                        gamePlay.input[boardKey].innerText = "X";
+                        Gameboard.gameboard[boardKey] = "X";
+                      }else{
+                        gamePlay.input[boardKey].innerText = "O";
+                        Gameboard.gameboard[boardKey] = "O";
+                   };
+                   gamePlay.xo += 1;
+                   
+                  }
                 }
-              }
-            },
-            
-           };
+                  // console.log(gamePlay.xo);
+                  // console.log(event.target.dataset);
+                  // console.log(event.target.dataset);
+              },
+              reset:()=>{
+                console.log(gamePlay.gameWon)
+                for(let i=0; i<gamePlay.input.length;i+=1){
+                  gamePlay.input[i].innerText = "";
+                }
+                gamePlay.gameWon = false;
+                
+                
+              },
+              startGame:()=>{
+                
+                  if(gamePlay.gameWon === false){
+                  for( let i=0; i<gamePlay.input.length; i+=1){
+                    gamePlay.input[i].addEventListener("click", gamePlay.addInput);
+                    gamePlay.input[i].addEventListener("click", gamePlay.checkWinner);
+                  }
+                }
+                
+                
+                
+              },
+          };
 
-// const gameBox = document.getElementsByClassName("game-box");
-
-const input = document.querySelectorAll(".input");
+// const gameBox : document.getElementsByClassName("game-box");
 
 
-const addInput = (event) => {
-    const boardKey = event.target.getAttribute("data-key");
-    // console.log(boardKey);
 
-    // gamePlay.checkWinner();
-    
-    if(input[boardKey].innerHTML === ""){
-        if(gamePlay.xo%2 === 0){
-          input[boardKey].innerText = "X";
-          Gameboard.gameboard[boardKey] = "X";
-        }else{
-          input[boardKey].innerText = "O";
-          Gameboard.gameboard[boardKey] = "O";
-     }
-     gamePlay.xo += 1;
-     
-    }
-    // console.log(gamePlay.xo);
-    // console.log(event.target.dataset);
-    // console.log(event.target.dataset);
 
-}
-
-for( let i=0; i<input.length; i+=1){
-    input[i].addEventListener("click", addInput);
-    input[i].addEventListener("click", gamePlay.checkWinner);
-    
-}
 
 
 
