77cb6466be58fad3654288fa81e4418a6497afe8
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-06-01 12:49:25 +0100

    created addTask DOM function

--- ToDo-list/src/dom.js
+++ ToDo-list/src/dom.js
@@ -1,4 +1,125 @@
+// Create Project and add to DOM
+
+import { projectNames, projects  } from "./index";
+
+// Add Project
+const addProjectFunc = ()=>{
+    const projectTitleInput = document.getElementById("project-title");
+    const newProjectName = projectTitleInput.value;
+    projectNames.push(newProjectName)
+
+    for (let i=0;i<projectNames.length; i+=1){
+        const name = projectNames[i];
+          projects[name] = [];
+        }
+
+    console.log(projects)
+
+    return newProjectName
+}
+ const addProjectDOM = (projectHTMLname)=>{
+    // Add to navbar
+    const navList = document.getElementById("nav");
+    const projectLi = document.createElement("li");
+    projectLi.className = "project";
+    projectLi.innerHTML = projectHTMLname;
+
+    navList.insertBefore(projectLi, navList.children[3+projectNames.length])
+
+    // Add to project dropdown
+    const projDropdown =document.getElementById("project-drpdwn");
+    const projDropdownName = document.createElement("a")
+    projDropdownName.innerHTML = projectHTMLname;
+    projDropdown.insertBefore(projDropdownName,projDropdown.children[1+projectNames.length])
+
+}
+
+
+// Add Project Section
+const addProjectText = document.getElementById("add-project-text");
+const addProjectDiv = document.getElementById("add-project-name");
+const addProjectButton = document.getElementById("add-new-project");
+
+// To-do Input form
+const inputForm = document.getElementById("input-form")
+const overlay = document.getElementById("overlay")
+const cancelInputButton = document.getElementById("cancel-input")
 
 
 
-// Add new project to DOM
+
+
+const addToDoButton = document.getElementById("add-to-do");
+
+const bringUpInputForm = ()=>{
+    overlay.classList.remove("hidden");
+    inputForm.classList.remove("hidden");
+}
+
+addToDoButton.addEventListener("click",bringUpInputForm);
+
+
+
+
+ const addProject = () => {
+    addProjectText.classList.add("hidden");
+    addProjectDiv.classList.remove("hidden");
+}
+
+const addNewProject = ()=>{
+    addProjectDiv.classList.add("hidden");
+    addProjectText.classList.remove("hidden")
+
+    addProjectDOM(addProjectFunc())
+}
+
+
+addProjectText.addEventListener("click",addProject);
+addProjectButton.addEventListener("click", addNewProject);
+
+// STOP HIAAA
+
+const cancelInput = ()=>{
+    inputForm.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+cancelInputButton.addEventListener("click",cancelInput);
+
+// CREATE task and add to DOM
+export const createTaskDOM= (title, dueDate, priority)=>{
+    const ToDoListDOM = document.getElementById("to-do-list");
+    const taskContainer = document.createElement("div");
+    taskContainer.className = "to-do-item";
+    const taskContainerHalf = document.createElement("div")
+    taskContainerHalf.className = "item-title-half";
+    taskContainer.appendChild(taskContainerHalf);
+    const taskTitle = document.createElement("p");
+    taskTitle.className = "item-title";
+    taskTitle.innerHTML = title
+    taskContainerHalf.appendChild(taskTitle);
+  
+    const dateTrash = document.createElement("div");
+    dateTrash.className = "date-trash";
+    const trashIcon = document.createElement("span");
+    trashIcon.className = "material-symbols-outlined";
+    trashIcon.innerHTML = "delete";
+    dateTrash.appendChild(trashIcon);
+    const dateHTML = document.createElement("p");
+    dateHTML.className = "date";
+    dateHTML.innerHTML = dueDate;
+    dateTrash.appendChild(dateHTML);
+    taskContainerHalf.appendChild(dateTrash);
+  
+    const checkboxPriorityDOM = document.createElement("div");
+    checkboxPriorityDOM.className = "checkbox-priority-half";
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxPriorityDOM.appendChild(checkboxInput);
+    const priorityHTML = document.createElement("p");
+    priorityHTML.innerHTML = priority;
+    checkboxPriorityDOM.appendChild(priorityHTML);
+    taskContainer.appendChild(checkboxPriorityDOM)
+    
+    ToDoListDOM.appendChild(taskContainer);
+  }
--- ToDo-list/src/index.js
+++ ToDo-list/src/index.js
@@ -1,118 +1,35 @@
 import "./style.css";
+import { createTaskDOM } from "./dom";
 
 // const tester = document.getElementById("home")
 // tester.addEventListener("click", ()=>{console.log(projects)})
 
-function createToDoItem(title, description, dueDate, priority, complete) {
-    return { title, description, dueDate, priority, complete };
-  }
-  const AllToDoItems = [];
-  
-
-const projectNames = ["Gym", "Work"]
-
-const projects = {}
-
-
-//Add Project
-const addProjectFunc = ()=>{
-    const projectTitleInput = document.getElementById("project-title");
-const newProjectName = projectTitleInput.value;
-    projectNames.push(newProjectName)
-
-    for (let i=0;i<projectNames.length; i+=1){
-        const name = projectNames[i];
-          projects[name] = [];
-        }
-
-    console.log(projects)
-    return newProjectName
-}
-const addProjectDOM = (projectHTMLname)=>{
-    const navList = document.getElementById("nav");
-    const projectLi = document.createElement("li");
-    projectLi.className = "project";
-    projectLi.innerHTML = projectHTMLname;
-
-    navList.insertBefore(projectLi, navList.children[3+projectNames.length])
+let TaskId = 1;
+function createToDoItem(title, description, dueDate, priority,project, complete) {
+  const id = TaskId
+  return { title, id, description, dueDate, priority,project, complete };
 }
 
-// for(let i=0; i<projectNameHtmls; i+=1){
-//     console.log(projectNameHtmls[i].innerHTML)
-//     projectNames.push(projectNameHtmls[i].innerHTML);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Add Project Section
-const addProjectText = document.getElementById("add-project-text");
-const addProjectDiv = document.getElementById("add-project-name");
-const addProjectButton = document.getElementById("add-new-project");
-
-// To-do Input form
-const inputForm = document.getElementById("input-form")
-const overlay = document.getElementById("overlay")
-const cancelInputButton = document.getElementById("cancel-input")
-
-
-
-
-
-const addToDoButton = document.getElementById("add-to-do");
-
-const bringUpInputForm = ()=>{
-    overlay.classList.remove("hidden");
-    inputForm.classList.remove("hidden");
+// Main task creator factory which changes task ID
+function createTask(title, description, dueDate, priority, complete){
+TaskId += 1;  
+  return createToDoItem(title, description, dueDate, priority, complete)
 }
 
-addToDoButton.addEventListener("click",bringUpInputForm);
+const AllToDoItems = [];
+
+
+export const projectNames = ["Gym", "Work"]
+
+export const projects = {}
+
+
+// adding task to DOM
+
+
+
+createTaskDOM("BAD5",1994,"Low");
 
 
 
 
-const addProject = () => {
-    addProjectText.classList.add("hidden");
-    addProjectDiv.classList.remove("hidden");
-}
-
-const addNewProject = ()=>{
-    addProjectDiv.classList.add("hidden");
-    addProjectText.classList.remove("hidden")
-
-    addProjectDOM(addProjectFunc())
-}
-
-
-addProjectText.addEventListener("click",addProject);
-addProjectButton.addEventListener("click", addNewProject);
-
-
-
-const cancelInput = ()=>{
-    inputForm.classList.add("hidden");
-    overlay.classList.add("hidden");
-}
-
-cancelInputButton.addEventListener("click",cancelInput);
