ae87db3880f877c2f89dc10275ca6fb48d6d29e5
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-06-08 18:40:09 +0100

    created objecrt storage for task values

--- ToDo-list/src/dom.js
+++ ToDo-list/src/dom.js
@@ -1,125 +1,152 @@
 // Create Project and add to DOM
 
-import { projectNames, projects  } from "./index";
+
 
 // Add Project
-const addProjectFunc = ()=>{
-    const projectTitleInput = document.getElementById("project-title");
-    const newProjectName = projectTitleInput.value;
-    projectNames.push(newProjectName)
+export const addProjectFunc = (projectNamesArray, projectsObject) => {
+  const projectTitleInput = document.getElementById("project-title");
+  const newProjectName = projectTitleInput.value;
+  console.log(newProjectName);
+  projectNamesArray.push(newProjectName);
 
-    for (let i=0;i<projectNames.length; i+=1){
-        const name = projectNames[i];
-          projects[name] = [];
-        }
+  for (let i = 0; i < projectNamesArray.length; i += 1) {
+    const name = projectNamesArray[i];
+    // eslint-disable-next-line no-param-reassign
+    projectsObject[name] = {};
+  }
 
-    console.log(projects)
+  console.log(projectsObject);
 
-    return newProjectName
-}
- const addProjectDOM = (projectHTMLname)=>{
-    // Add to navbar
-    const navList = document.getElementById("nav");
-    const projectLi = document.createElement("li");
-    projectLi.className = "project";
-    projectLi.innerHTML = projectHTMLname;
+  return newProjectName;
+};
+export const addProjectDOM = (projectHTMLname, projectNamesArray) => {
+  // Add to navbar
+  const navList = document.getElementById("nav");
+  const projectLi = document.createElement("li");
+  projectLi.className = "project";
+  projectLi.innerHTML = projectHTMLname;
 
-    navList.insertBefore(projectLi, navList.children[3+projectNames.length])
-
-    // Add to project dropdown
-    const projDropdown =document.getElementById("project-drpdwn");
-    const projDropdownName = document.createElement("a")
-    projDropdownName.innerHTML = projectHTMLname;
-    projDropdown.insertBefore(projDropdownName,projDropdown.children[1+projectNames.length])
-
-}
-
-
-// Add Project Section
-const addProjectText = document.getElementById("add-project-text");
-const addProjectDiv = document.getElementById("add-project-name");
-const addProjectButton = document.getElementById("add-new-project");
-
-// To-do Input form
-const inputForm = document.getElementById("input-form")
-const overlay = document.getElementById("overlay")
-const cancelInputButton = document.getElementById("cancel-input")
-
-
-
-
-
-const addToDoButton = document.getElementById("add-to-do");
-
-const bringUpInputForm = ()=>{
-    overlay.classList.remove("hidden");
-    inputForm.classList.remove("hidden");
-}
-
-addToDoButton.addEventListener("click",bringUpInputForm);
-
-
-
-
- const addProject = () => {
-    addProjectText.classList.add("hidden");
-    addProjectDiv.classList.remove("hidden");
-}
-
-const addNewProject = ()=>{
-    addProjectDiv.classList.add("hidden");
-    addProjectText.classList.remove("hidden")
-
-    addProjectDOM(addProjectFunc())
-}
-
-
-addProjectText.addEventListener("click",addProject);
-addProjectButton.addEventListener("click", addNewProject);
-
-// STOP HIAAA
-
-const cancelInput = ()=>{
-    inputForm.classList.add("hidden");
-    overlay.classList.add("hidden");
-}
-
-cancelInputButton.addEventListener("click",cancelInput);
+  navList.insertBefore(
+    projectLi,
+    navList.children[1 + projectNamesArray.length]
+  );
+          
+  // Add to project dropdown
+  const projDropdown = document.getElementById("project-drpdwn");
+  const projDropdownName = document.createElement("a");
+  projDropdownName.className = "proj"
+  projDropdownName.innerHTML = projectHTMLname;
+  projDropdown.insertBefore(
+    projDropdownName,
+    projDropdown.children[1 + projectNamesArray.length]
+  );
+  
+};
 
 // CREATE task and add to DOM
-export const createTaskDOM= (title, dueDate, priority)=>{
-    const ToDoListDOM = document.getElementById("to-do-list");
-    const taskContainer = document.createElement("div");
-    taskContainer.className = "to-do-item";
-    const taskContainerHalf = document.createElement("div")
-    taskContainerHalf.className = "item-title-half";
-    taskContainer.appendChild(taskContainerHalf);
-    const taskTitle = document.createElement("p");
-    taskTitle.className = "item-title";
-    taskTitle.innerHTML = title
-    taskContainerHalf.appendChild(taskTitle);
-  
-    const dateTrash = document.createElement("div");
-    dateTrash.className = "date-trash";
-    const trashIcon = document.createElement("span");
-    trashIcon.className = "material-symbols-outlined";
-    trashIcon.innerHTML = "delete";
-    dateTrash.appendChild(trashIcon);
-    const dateHTML = document.createElement("p");
-    dateHTML.className = "date";
-    dateHTML.innerHTML = dueDate;
-    dateTrash.appendChild(dateHTML);
-    taskContainerHalf.appendChild(dateTrash);
-  
-    const checkboxPriorityDOM = document.createElement("div");
-    checkboxPriorityDOM.className = "checkbox-priority-half";
-    const checkboxInput = document.createElement("input");
-    checkboxInput.type = "checkbox";
-    checkboxPriorityDOM.appendChild(checkboxInput);
-    const priorityHTML = document.createElement("p");
-    priorityHTML.innerHTML = priority;
-    checkboxPriorityDOM.appendChild(priorityHTML);
-    taskContainer.appendChild(checkboxPriorityDOM)
+export const createTaskDOM = (title, dueDate, priority) => {
+  const ToDoListDOM = document.getElementById("to-do-list");
+  const taskContainer = document.createElement("div");
+  taskContainer.className = "to-do-item";
+  const taskContainerHalf = document.createElement("div");
+  taskContainerHalf.className = "item-title-half";
+  taskContainer.appendChild(taskContainerHalf);
+  const taskTitle = document.createElement("p");
+  taskTitle.className = "item-title";
+  taskTitle.innerHTML = title;
+  taskContainerHalf.appendChild(taskTitle);
+
+  const dateTrash = document.createElement("div");
+  dateTrash.className = "date-trash";
+  const trashIcon = document.createElement("span");
+  trashIcon.className = "material-symbols-outlined";
+  trashIcon.innerHTML = "delete";
+  dateTrash.appendChild(trashIcon);
+  const dateHTML = document.createElement("p");
+  dateHTML.className = "date";
+  dateHTML.innerHTML = dueDate;
+  dateTrash.appendChild(dateHTML);
+  taskContainerHalf.appendChild(dateTrash);
+
+  const checkboxPriorityDOM = document.createElement("div");
+  checkboxPriorityDOM.className = "checkbox-priority-half";
+  const checkboxInput = document.createElement("input");
+  checkboxInput.type = "checkbox";
+  checkboxPriorityDOM.appendChild(checkboxInput);
+  const priorityHTML = document.createElement("p");
+  priorityHTML.innerHTML = priority;
+  checkboxPriorityDOM.appendChild(priorityHTML);
+  taskContainer.appendChild(checkboxPriorityDOM);
+
+  ToDoListDOM.appendChild(taskContainer);
+};
+
+// Priority dropdown function
+const priorityDrpdwn = document.getElementById("priority");
+const lvls = document.querySelectorAll(".lvl");
+
+function getPriorityStyle(levelText){
+    if(levelText === "Low"){
+        priorityDrpdwn.setAttribute("style","background-color: rgb(0,255,0); color:white")
+    }else if(levelText ==="Moderate"){
+        priorityDrpdwn.setAttribute("style","background-color: rgb(255, 145, 0); color:white")
+    }else if(levelText==="High"){
+        priorityDrpdwn.setAttribute("style","background-color: rgb(255, 37, 37); color:white")
+    }
     
-    ToDoListDOM.appendChild(taskContainer);
-  }
+}
+
+export const selectPriority = () => {
+  lvls.forEach((lvl) => {
+    lvl.addEventListener("click", () => {
+      priorityDrpdwn.innerHTML = lvl.innerHTML;
+      getPriorityStyle(lvl.innerHTML);
+      
+    });
+  });
+};
+// select Project Dropdown
+
+export const selectProject = ()=>{
+    const projectDrpdwn = document.getElementById("project-btn");
+    const projs = document.querySelectorAll(".proj");
+
+    console.log("9");
+    projs.forEach((proj)=>{
+        proj.addEventListener("click",()=>{
+            projectDrpdwn.innerHTML = proj.innerHTML;
+        })
+    })
+    
+}
+ 
+// function getTaskInfo() {
+//   const taskID = TaskId
+//   // const projectName = projectNameX;
+//   const taskTitle = document.getElementById("title").value;
+//   const taskDescription = document.getElementById("description-input").value;
+//   const dateValue = document.getElementById("due-date").value;
+//   const priorityValue = document.getElementById("priority").innerText;
+//   const projectValue = document.getElementById("project-btn").innerHTML;
+//   const complete = false;
+
+//   return {taskID, taskTitle, taskDescription, dateValue, priorityValue, projectValue, complete}
+// }
+
+
+// function saveTasktoProject(){
+//     const ${`task`id}
+//     (function(){
+
+//     }())
+
+// }
+
+export const clearFormInfo = () => {
+  document.getElementById("title").value = "";
+  document.getElementById("description-input").value = "";
+  document.getElementById("due-date").value = "2030-07-22";
+  document.getElementById("priority").innerHTML = "Low";
+  document.getElementById("priority").style.backgroundColor="rgb(25, 239, 25)"
+  document.getElementById("project-btn").innerHTML = "Today";
+};
--- ToDo-list/src/index.js
+++ ToDo-list/src/index.js
@@ -1,35 +1,120 @@
 import "./style.css";
-import { createTaskDOM } from "./dom";
+import {
+  createTaskDOM,
+  addProjectDOM,
+  addProjectFunc,
+  selectPriority,
+  clearFormInfo,
+  selectProject,
+} from "./dom";
 
 // const tester = document.getElementById("home")
 // tester.addEventListener("click", ()=>{console.log(projects)})
+const saveButton = document.getElementById("save");
 
 let TaskId = 1;
-function createToDoItem(title, description, dueDate, priority,project, complete) {
-  const id = TaskId
-  return { title, id, description, dueDate, priority,project, complete };
+const  getTaskInfo = ()=>{
+  const taskID = TaskId
+  // const projectName = projectNameX;
+  const taskTitle = document.getElementById("title").value;
+  const taskDescription = document.getElementById("description-input").value;
+  const dateValue = document.getElementById("due-date").value;
+  const priorityValue = document.getElementById("priority").innerText;
+  const projectValue = document.getElementById("project-btn").innerHTML;
+  const complete = false;
+
+  return {taskID, taskTitle, taskDescription, dateValue, priorityValue, projectValue, complete}
 }
+
+
+const AllToDoItems = {};
+
+export const projectNames = ["Home", "Today", "Gym", "Work"];
+
+// Projects object
+export const projects = {};
 
 // Main task creator factory which changes task ID
-function createTask(title, description, dueDate, priority, complete){
-TaskId += 1;  
-  return createToDoItem(title, description, dueDate, priority, complete)
+function createTask() {
+  const ATask = getTaskInfo()
+  
+
+  TaskId += 1;
+  return ATask;
+}
+const tester = document.getElementById("home")
+tester.addEventListener("click", ()=>{console.log(createTask())})
+
+// Save task object to project 
+function saveTasktoProject(){
+ const projectsName=document.getElementById("project-btn").innerHTML
+ projects[projectsName] = createTask();
+//  console.log(projects)
+    // First create taskID object
+    // eslint-disable-next-line no-param-reassign
+    // projectsName[`task${TaskId}`] = createTask();
+    
+    console.log(projects);
+
 }
 
-const AllToDoItems = [];
+// Add Project Section
+const addProjectText = document.getElementById("add-project-text");
+const addProjectDiv = document.getElementById("add-project-name");
+const addProjectButton = document.getElementById("add-new-project");
+
+// To-do Input form
+const inputForm = document.getElementById("input-form");
+const overlay = document.getElementById("overlay");
+const cancelInputButton = document.getElementById("cancel-input");
+const priorityDrpdwn = document.getElementById("priority");
+
+const addToDoButton = document.getElementById("add-to-do");
+
+const bringUpInputForm = () => {
+  overlay.classList.remove("hidden");
+  inputForm.classList.remove("hidden");
+  
+};
+
+addToDoButton.addEventListener("click", bringUpInputForm);
+
+const addProject = () => {
+  addProjectText.classList.add("hidden");
+  addProjectDiv.classList.remove("hidden");
+};
+
+const addNewProject = () => {
+  addProjectDiv.classList.add("hidden");
+  addProjectText.classList.remove("hidden");
+
+  addProjectDOM(addProjectFunc(projectNames, projects), projectNames);
+//   add projs to input dropdown
+  selectProject();
+};
+
+addProjectText.addEventListener("click", addProject);
+addProjectButton.addEventListener("click", addNewProject);
+
+// Cancel / close form
+const cancelInput = () => {
+  inputForm.classList.add("hidden");
+  overlay.classList.add("hidden");
+  clearFormInfo();
+};
+
+cancelInputButton.addEventListener("click", cancelInput);
+
+// Set priority and change bg color
+selectPriority();
+
+// Set Project 
+selectProject();
 
 
-export const projectNames = ["Gym", "Work"]
-
-export const projects = {}
-
+// Input Save Buttton
+saveButton.addEventListener("click",saveTasktoProject)
 
 // adding task to DOM
-
-
-
-createTaskDOM("BAD5",1994,"Low");
-
-
-
+createTaskDOM("BAD5", 1994, "Low9");
 
