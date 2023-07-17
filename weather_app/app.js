d6dc2bf77795064f14cd00d4c25a42f9b6db581e
Kingsley <72080248+KingsleyND@users.noreply.github.com>
2023-07-17 23:52:33 +0100

    finished search function

--- weather_app/app.js
+++ weather_app/app.js
@@ -17,10 +17,26 @@ const get12hTime = (time24h)=>{
 
 console.log(get12hTime("23:09"))
 
+// Gif generator
+async function fetchGif(word){
+    const res = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=gnyJpy9CPOaN30eY3pzmiTh73la554NT&s=${word}`)
+    if (res.status === 200){
+    const gifData = await res.json();
+    console.log(gifData)
+        const gifSrc =  gifData.data.images.original.url; 
+        console.log(gifSrc)
+        return gifSrc ;
+        
+    }
+    
+      throw new Error(res.status) 
+};
 
+//  console.log(fetchGif("wunna"))
+// https://media2.giphy.com/media/iiPX68eBIBNSzTNfl4/giphy.gif?cid=2eb775f7rpffpoccohbdxbjwn7nxvsqzsxtsb8xsaq4kp86j&ep=v1_gifs_translate&rid=giphy.gif&ct=g
 
 async function getWeather(place){
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=da8b7883c24a41ad96493418231007&q=cape town")
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=da8b7883c24a41ad96493418231007&q=${place}`)
     const weatherData = await response.json()
 
     const location = `${weatherData.location.name}, ${weatherData.location.country}`;
@@ -34,10 +50,11 @@ async function getWeather(place){
     
     return{location, placeCondition, conditionIcon, weatherData, degree, locationTime}
 }
+
 async function getData(){
     const locationInput = document.getElementById("location-input").value
-    const weatherPlace = getWeather();
-
+    const weatherPlace = getWeather(locationInput);
+    
     const placeText = document.getElementById("place-title");
     placeText.innerText = (await weatherPlace).location
     const locationTime = document.getElementById("time");
@@ -48,8 +65,9 @@ async function getData(){
     locatiionDegree.innerText = (await weatherPlace).degree;
     const weatherIcon = document.getElementById("icon");
     weatherIcon.src = (await weatherPlace).conditionIcon;
-    const weatherGif = document.getElementById("weather-gif")
-
+    const weatherGif = document.getElementById("weather-gif");  
+    weatherGif.src = (await fetchGif(`${(await weatherPlace).placeCondition} weather`));
+    
     console.log((await weatherPlace).conditionIcon)
     console.log((await weatherPlace).weatherData)
 }
